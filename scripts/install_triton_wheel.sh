0c755c835dffdae1ec361c2d3bc23bad5061a46c
Digant Desai <digantdesai@meta.com>
2024-12-13 12:34:04 -0600

    [NOT_FOR_COMMIT] Try Triton-cpu-arm

--- scripts/install_triton_wheel.sh
+++ scripts/install_triton_wheel.sh
@@ -3,29 +3,37 @@
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 DOWNLOAD_PYTORCH_ORG="https://download.pytorch.org/whl"
 
-if [[ -z "${USE_XPU}" ]]; then
-    # Default install from PyTorch source
-
-    TRITON_VERSION="pytorch-triton==$(cat .ci/docker/triton_version.txt)"
-    if [[ "$BRANCH" =~ .*release.* ]]; then
-        pip install --index-url ${DOWNLOAD_PYTORCH_ORG}/test/ $TRITON_VERSION
-    else
-        pip install --index-url ${DOWNLOAD_PYTORCH_ORG}/nightly/ $TRITON_VERSION+git$(head -c 8 .ci/docker/ci_commit_pins/triton.txt)
-    fi
-else
-    # The Triton xpu logic is as follows:
-    # 1. By default, install pre-built whls.
-    # 2. [Not exposed to user] If the user set `TRITON_XPU_BUILD_FROM_SOURCE=1` flag,
-    #    it will install Triton from the source.
-
-    TRITON_VERSION="pytorch-triton-xpu==$(cat .ci/docker/triton_version.txt)"
-    TRITON_XPU_COMMIT_ID="git$(head -c 8 .ci/docker/ci_commit_pins/triton-xpu.txt)"
-    if [[ -z "${TRITON_XPU_BUILD_FROM_SOURCE}" ]]; then
-        pip install --index-url ${DOWNLOAD_PYTORCH_ORG}/nightly/ ${TRITON_VERSION}+${TRITON_XPU_COMMIT_ID}
-    else
-        TRITON_XPU_REPO="https://github.com/intel/intel-xpu-backend-for-triton"
-
+# if [[ -z "${USE_XPU}" ]]; then
+#     # Default install from PyTorch source
+# 
+#     TRITON_VERSION="pytorch-triton==$(cat .ci/docker/triton_version.txt)"
+#     if [[ "$BRANCH" =~ .*release.* ]]; then
+#         pip install --index-url ${DOWNLOAD_PYTORCH_ORG}/test/ $TRITON_VERSION
+#     else
+#         pip install --index-url ${DOWNLOAD_PYTORCH_ORG}/nightly/ $TRITON_VERSION+git$(head -c 8 .ci/docker/ci_commit_pins/triton.txt)
+#     fi
+# else
+#     # The Triton xpu logic is as follows:
+#     # 1. By default, install pre-built whls.
+#     # 2. [Not exposed to user] If the user set `TRITON_XPU_BUILD_FROM_SOURCE=1` flag,
+#     #    it will install Triton from the source.
+#     TRITON_XPU_BUILD_FROM_SOURCE=1
+#     TRITON_VERSION="pytorch-triton-xpu==$(cat .ci/docker/triton_version.txt)"
+#     TRITON_XPU_COMMIT_ID="git$(head -c 8 .ci/docker/ci_commit_pins/triton-xpu.txt)"
+#     if [[ -z "${TRITON_XPU_BUILD_FROM_SOURCE}" ]]; then
+#         pip install --index-url ${DOWNLOAD_PYTORCH_ORG}/nightly/ ${TRITON_VERSION}+${TRITON_XPU_COMMIT_ID}
+#     else
+        TRITON_REPO="https://github.com/triton-lang/triton-cpu"
+        TRITON_COMMIT_ID="$(cat .ci/docker/ci_commit_pins/triton-cpu.txt)"
+        echo "TRITON_REPO: ${TRITON_REPO}"
+        echo "TRITON_COMMIT_ID: ${TRITON_COMMIT_ID}"
         # force-reinstall to ensure the pinned version is installed
-        pip install --force-reinstall "git+${TRITON_XPU_REPO}@${TRITON_XPU_COMMIT_ID}#subdirectory=python"
-    fi
-fi
+        git clone --recursive ${TRITON_REPO} triton
+        cd triton
+        git checkout ${TRITON_COMMIT_ID}
+        git submodule update --init --recursive
+        cd python
+        CC=$(which clang) \
+        pip install .
+#     fi
+# fi
