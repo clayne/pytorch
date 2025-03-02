clayne/pytorch

commit 3b4c711e36103c72711529d52e69b16f95ebe5fe

[AOTI][refactor] Consolidate CppBuilder.build and CppBuilder.build_fbcode (#147975)
Summary: Let CppBuilder handle all the cpp build logic

Differential Revision: D70141808

Pull Request resolved: https://github.com/pytorch/pytorch/pull/147975
Approved by: https://github.com/angelayi, https://github.com/yushangdi

torch/_inductor/codecache.py
@@ -1423,12 +1423,6 @@ class AotCodeCompiler:
         # guarantee the source code hash contains ISA difference.
         cpp_command = repr(vec_isa_cmd_gen.get_command_line())
 
-        # Meta internal AOTInductor CPU
-        fbcode_aot_cpu_re = (
-            config.is_fbcode() and device_type == "cpu" and graph.aot_mode
-        )
-        use_relative_path = fbcode_aot_cpu_re
-
         (
             specified_output_path,
             specified_artifact_name,
@@ -1512,7 +1506,7 @@ class AotCodeCompiler:
                 device_type=device_type if device_type != "xpu" else "cpu",
                 aot_mode=graph.aot_mode,
                 compile_only=True,
-                use_relative_path=use_relative_path,
+                use_relative_path=config.is_fbcode(),
             )
             object_builder = CppBuilder(
                 name=str(consts_s.stem),
@@ -1521,13 +1515,7 @@ class AotCodeCompiler:
                 BuildOption=object_build_options,
             )
             consts_o = object_builder.get_target_file_path()
-            if fbcode_aot_cpu_re:
-                # TODO: refactor fbcode_aot_cpu_re logic into CppBuilder
-                consts_o = str(consts_s.with_suffix(".o"))
-                object_builder.build_fbcode(str(consts_s), consts_o)
-                os.chmod(consts_o, 0o644)
-            else:
-                object_builder.build()
+            object_builder.build()
 
             if is_large_consts:
                 with open(consts_o, "r+b") as f:
@@ -1640,7 +1628,7 @@ class AotCodeCompiler:
                 device_type=device_type,
                 aot_mode=graph.aot_mode,
                 compile_only=True,
-                use_relative_path=use_relative_path,
+                use_relative_path=config.is_fbcode(),
                 use_mmap_weights=use_mmap_weights,
             )
             object_builder = CppBuilder(
@@ -1666,12 +1654,7 @@ class AotCodeCompiler:
                 object_builder.save_src_to_cmake(cmake_path, cpp_path)
                 generated_files.append(cmake_path)
             else:
-                if fbcode_aot_cpu_re:
-                    output_o = str(cpp_path_operator.with_suffix(".o"))
-                    object_builder.build_fbcode(cpp_path, output_o)
-                    os.chmod(output_o, 0o644)
-                else:
-                    object_builder.build()
+                object_builder.build()
 
             if not use_mmap_weights:
                 aot_constants = serialized_weights
@@ -1698,12 +1681,14 @@ class AotCodeCompiler:
                 vec_isa=picked_vec_isa,
                 device_type=device_type,
                 aot_mode=graph.aot_mode,
-                use_relative_path=use_relative_path,
+                use_relative_path=config.is_fbcode(),
             )
 
             so_builder = CppBuilder(
                 name=output_name,
-                sources=[output_o, consts_o, kernels_o],
+                sources=[output_o, consts_o, kernels_o]
+                if kernels_o
+                else [output_o, consts_o],
                 output_dir=output_dir,
                 BuildOption=so_build_options,
             )
@@ -1750,16 +1735,7 @@ class AotCodeCompiler:
                     so_builder.save_src_to_cmake(cmake_path, kernel_o)
                 so_builder.save_link_cmd_to_cmake(cmake_path)
             else:
-                if fbcode_aot_cpu_re:
-                    output_so = (
-                        config.aot_inductor.output_path
-                        if specified_artifact_name
-                        else str(cpp_path_operator.with_suffix(".so"))
-                    )
-                    so_builder.build_fbcode([output_o, consts_o], output_so)
-                    os.chmod(output_so, 0o755)
-                else:
-                    so_builder.build()
+                so_builder.build()
 
                 for o_file in [
                     output_o,
@@ -1938,35 +1914,24 @@ class CppCodeCache:
 
             lock_path = os.path.join(get_lock_dir(), key + ".lock")
             output_name, output_dir = get_name_and_dir_from_output_file_path(input_path)
-            """
-            If `fb_code` env, it need to be dispatched to original `compile_file` function.
-            So, we still need to prepare parameters for the function: `input_path` and `fb_output_path`.
-            """
-            fb_output_path = input_path[:-3] + "so"
             future: Optional[Future[Any]] = None
             lib = None
 
-            cpp_build_option = CppTorchDeviceOptions(**compile_command)
+            cpp_build_option = CppTorchDeviceOptions(
+                **compile_command, use_relative_path=config.is_fbcode()
+            )
             cpp_builder = CppBuilder(
                 name=output_name,
                 sources=input_path,
                 output_dir=output_dir,
                 BuildOption=cpp_build_option,
             )
-
             worker_fn = functools.partial(
                 _worker_compile_cpp,
                 lock_path,
                 cpp_builder,
-                input_path,
-                fb_output_path,
             )
-
-            binary_path = normalize_path_separator(
-                fb_output_path
-                if config.is_fbcode()
-                else cpp_builder.get_target_file_path()
-            )
+            binary_path = normalize_path_separator(cpp_builder.get_target_file_path())
 
             def load_fn() -> Any:
                 nonlocal lib
@@ -1996,23 +1961,12 @@ class CppCodeCache:
 def _worker_compile_cpp(
     lock_path: str,
     cpp_builder: CppBuilder,
-    fb_input_path: str,
-    fb_output_path: str,
 ) -> None:
     from torch.utils._filelock import FileLock
 
     with FileLock(lock_path, timeout=LOCK_TIMEOUT):
-        binary_path = (
-            fb_output_path if config.is_fbcode() else cpp_builder.get_target_file_path()
-        )
-        if not os.path.exists(binary_path):
-            if config.is_fbcode():
-                cpp_builder.build_fbcode(
-                    fb_input_path,
-                    fb_output_path,
-                )
-            else:
-                cpp_builder.build()
+        if not os.path.exists(cpp_builder.get_target_file_path()):
+            cpp_builder.build()
 
 
 # Customized Python binding for cpp kernels

torch/_inductor/cpp_builder.py
@@ -1394,6 +1394,8 @@ class CppBuilder:
         self._libraries_args = ""
         self._passthrough_parameters_args = ""
 
+        # When relative path is used, we need to maintain the source dir list.
+        self._orig_source_paths = []
         self._output_dir = ""
         self._target_file = ""
 
@@ -1428,8 +1430,7 @@ class CppBuilder:
                 # Will create another temp director for building, so do NOT use
                 # use the absolute path.
                 inp_name = [os.path.basename(i) for i in sources]
-                self._target_file = os.path.basename(self._target_file)
-
+                self._orig_source_paths = sources
             self._sources_args = " ".join(inp_name)
         else:
             self._sources_args = " ".join(sources)
@@ -1516,34 +1517,27 @@ class CppBuilder:
             libraries_args=self._libraries_args,
             libraries_dirs_args=self._libraries_dirs_args,
             passthrough_args=self._passthrough_parameters_args,
-            target_file=self._target_file,
+            target_file=os.path.basename(self._target_file)
+            if self._use_relative_path
+            else self._target_file,
         )
         return command_line
 
     def get_target_file_path(self) -> str:
         return normalize_path_separator(self._target_file)
 
-    # Given a path to an input cpp file and an output path,
-    # Attempts to compile the file, storing the output in "output_path"
-    def build_fbcode(
+    def build_fbcode_re(
         self,
-        input_path: Union[str, list[str]],
-        output_path: str,
     ) -> None:
         from torch._inductor.codecache import cpp_prefix_path
 
         with dynamo_timed("compile_file"):
-            input_paths = [input_path] if isinstance(input_path, str) else input_path
-            input_files = [
-                os.path.basename(ip) if config.is_fbcode() else ip for ip in input_paths
-            ]
             command = self.get_command_line().split()
             try:
-                assert config.is_fbcode(), "compile_file() is only used in fbcode"
                 # Need to copy our header into the same folder as the sourcecode.
                 header_path = cpp_prefix_path()
                 header_name = os.path.basename(header_path)
-                output_name = os.path.basename(output_path)
+                output_path = self._target_file
                 # When we build remotely, we need to make sure to carefully copy any files
                 # that are required during the compilation process into our build directly.
                 # This is where all of the ATen/c10/Torch includes come from.
@@ -1552,16 +1546,22 @@ class CppBuilder:
                     # Copy everything to tmp compilation folder
                     shutil.copy(header_path, os.path.join(tmp_dir, header_name))
                     shutil.copy(_LINKER_SCRIPT, os.path.join(tmp_dir, "script.ld"))
-                    for p, f in zip(input_paths, input_files):
-                        shutil.copy(p, os.path.join(tmp_dir, f))
+                    for src in self._orig_source_paths:
+                        shutil.copy(src, os.path.join(tmp_dir, os.path.basename(src)))
                     dest_include_path = os.path.join(tmp_dir, "include")
                     shutil.copytree(torch_includes_path, dest_include_path)
                     # Run the build
-                    output_file_path = _run_build_command(command, tmp_dir, output_name)
+                    tmp_output_path = _run_build_command(
+                        command, tmp_dir, os.path.basename(output_path)
+                    )
                     # Copy output from the build
                     if os.path.exists(output_path):
                         os.remove(output_path)
-                    shutil.copy(output_file_path, output_path)
+                    shutil.copy(tmp_output_path, output_path)
+                    if output_path.endswith(".o"):
+                        os.chmod(output_path, 0o644)
+                    elif output_path.endswith(".so"):
+                        os.chmod(output_path, 0o755)
             except subprocess.CalledProcessError as e:
                 output = e.output.decode("utf-8")
                 raise exc.CppCompileError(command, output) from e
@@ -1571,6 +1571,9 @@ class CppBuilder:
         It is must need a temperary directory to store object files in Windows.
         After build completed, delete the temperary directory to save disk space.
         """
+        if config.is_fbcode():
+            # remote build uses relative path
+            return self.build_fbcode_re()
         _create_if_dir_not_exist(self._output_dir)
         _build_tmp_dir = os.path.join(
             self._output_dir, f"{self._name}_{_BUILD_TEMP_DIR}"

commit df93527b83d30258a6e291173bf4a5d1aeee9922

[AOTI] Fix aot_inductor_package test errors
Summary: Fix fbcode test failures introduced by https://github.com/pytorch/pytorch/pull/147975. Make sure script.ld is copied to the build-time directory.

Differential Revision: D70454149

torch/_inductor/codecache.py
@@ -54,6 +54,7 @@ from torch._inductor.codegen.rocm.compile_command import (
     rocm_compiler,
 )
 from torch._inductor.cpp_builder import (
+    _LINKER_SCRIPT,
     _set_gpu_runtime_env,
     _TORCH_PATH,
     _transform_cuda_paths,
@@ -1710,6 +1711,7 @@ class AotCodeCompiler:
                 )
                 so_build_options.save_flags_to_json(linker_flags)
                 generated_files.append(linker_flags)
+                generated_files.append(_LINKER_SCRIPT)
 
                 # If we only want to package the cpp, then we need to save the
                 # weights separately into a bin, and we also need to prevent compiling the so

torch/_inductor/cpp_builder.py
@@ -723,10 +723,7 @@ def _setup_standard_sys_libs(
         include_dirs.append(build_paths.linux_kernel_include)
         include_dirs.append("include")
 
-        if aot_mode and not use_relative_path:
-            linker_script = _LINKER_SCRIPT
-        else:
-            linker_script = os.path.basename(_LINKER_SCRIPT)
+        linker_script = os.path.basename(_LINKER_SCRIPT)
 
         if _is_clang(cpp_compiler):
             passthrough_args.append(" --rtlib=compiler-rt")

torch/_inductor/package/package.py
@@ -2,8 +2,6 @@ import io
 import json
 import logging
 import os
-import shlex
-import subprocess
 import tempfile
 import zipfile
 from pathlib import Path
@@ -13,7 +11,7 @@ from typing_extensions import Self
 import torch
 import torch._inductor
 import torch.utils._pytree as pytree
-from torch._inductor import exc
+from torch._inductor import config
 from torch._inductor.cpp_builder import BuildOptionsBase, CppBuilder
 from torch.export._tree_utils import reorder_kwargs
 from torch.types import FileLike
@@ -92,14 +90,6 @@ class PT2ArchiveReader:
         return self.archive_file.namelist()
 
 
-def _run_command_and_check(cmd: str) -> None:
-    cmd = shlex.split(cmd)
-    try:
-        subprocess.run(cmd, check=True)
-    except subprocess.CalledProcessError as e:
-        raise exc.CppCompileError(cmd, e.output) from e
-
-
 def compile_so(aoti_dir: str, aoti_files: list[str], so_path: str) -> str:
     def get_aoti_file_with_suffix(suffix: str) -> str:
         for file in aoti_files:
@@ -117,32 +107,32 @@ def compile_so(aoti_dir: str, aoti_files: list[str], so_path: str) -> str:
     with open(file_name + "_compile_flags.json") as f:
         compile_flags = json.load(f)
 
-    compile_options = BuildOptionsBase(**compile_flags)
+    compile_options = BuildOptionsBase(
+        **compile_flags, use_relative_path=config.is_fbcode()
+    )
     object_builder = CppBuilder(
         name=file_name,
         sources=cpp_file,
         BuildOption=compile_options,
     )
-    compile_cmd = object_builder.get_command_line()
     output_o = object_builder.get_target_file_path()
-
-    _run_command_and_check(compile_cmd)
+    object_builder.build()
 
     # Parse linker flags and build the .so file
     with open(file_name + "_linker_flags.json") as f:
         linker_flags = json.load(f)
 
-    linker_options = BuildOptionsBase(**linker_flags)
+    linker_options = BuildOptionsBase(
+        **linker_flags, use_relative_path=config.is_fbcode()
+    )
     so_builder = CppBuilder(
         name=os.path.split(so_path)[-1],
         sources=[output_o, consts_o],
         BuildOption=linker_options,
         output_dir=so_path,
     )
-    link_cmd = so_builder.get_command_line()
     output_so = so_builder.get_target_file_path()
-
-    _run_command_and_check(link_cmd)
+    so_builder.build()
 
     # mmapped weights
     serialized_weights_filename = file_name + "_serialized_weights.bin"

torch/csrc/inductor/aoti_package/model_package_loader.cpp
@@ -98,6 +98,11 @@ std::tuple<std::string, std::string> get_cpp_compile_command(
 
   std::string file_ext = compile_only ? ".o" : ".so";
   std::string target_file = output_dir + filename + file_ext;
+  std::string target_dir = output_dir;
+  if (target_dir.empty()) {
+    size_t parent_path_idx = filename.find_last_of(k_separator);
+    target_dir = filename.substr(0, parent_path_idx);
+  }
 
   std::string cflags_args;
   for (auto& arg : compile_options["cflags"]) {
@@ -131,7 +136,15 @@ std::tuple<std::string, std::string> get_cpp_compile_command(
 
   std::string passthrough_parameters_args;
   for (auto& arg : compile_options["passthrough_args"]) {
-    passthrough_parameters_args += arg.get<std::string>() + " ";
+    std::string arg_str = arg.get<std::string>();
+    std::string target = "script.ld";
+    std::string replacement = target_dir;
+    replacement.append(k_separator).append(target);
+    size_t pos = arg_str.find(target);
+    if (pos != std::string::npos) {
+      arg_str.replace(pos, target.length(), replacement);
+    }
+    passthrough_parameters_args += arg_str + " ";
   }
 
   std::string compile_only_arg = compile_only ? "-c" : "";